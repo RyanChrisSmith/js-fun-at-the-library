80ecaaf8915844fbd9ce217124a56a9d2d8e4f37
Ryan Smith <106180616+RyanChrisSmith@users.noreply.github.com>
2023-02-26 12:44:07 -0700

    iteration 3 finished, getting into complicated looping and iterating

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,37 @@
+function createLibrary(name) {
+  return {
+    name: name,
+    shelves: {
+      fantasy: [],
+      fiction: [],
+      nonFiction: []
+    }
+  };
+}
+
+function addBook(library, book) {
+  library.shelves[book.genre].push(book);
+}
+
+function checkoutBook(library, title, genre) {
+  // Check if the book exists on the shelf
+  var bookIndex = library.shelves[genre].findIndex(function(book) {
+    return book.title === title;
+  });
+
+  // If book exists on the shelf, remove it from the shelf and return a success message
+  if (bookIndex !== -1) {
+    library.shelves[genre].splice(bookIndex, 1);
+    return "You have now checked out " + title + " from the " + library.name;
+  } else {
+    // If book does not exist on the shelf, return a failure message
+    return "Sorry, there are currently no copies of " + title + " available at the " + library.name;
+  }
+}
 
 
 module.exports = {
-  // createLibrary,
-  // addBook,
-  // checkoutBook
+  createLibrary,
+  addBook,
+  checkoutBook
 };
