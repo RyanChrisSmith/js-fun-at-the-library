8065e9b39fee4e625758438a499247e76d4b01da
Ryan Smith <106180616+RyanChrisSmith@users.noreply.github.com>
2023-02-26 15:07:00 -0700

    extension finished, integrated a librarian into the Library

--- src/librarian.js
+++ src/librarian.js
@@ -1,6 +1,31 @@
 class Librarian {
-  constructor() {
-    
+  constructor(name, library) {
+    this.name = name;
+    this.library = library;
+  }
+
+  greetPatron(name, isMorning) {
+    if (isMorning) {
+      return `Good morning, ${name}!`;
+    } else {
+      return `Hello, ${name}!`;
+    }
+  }
+
+  findBook(title) {
+    const shelves = this.library.shelves;
+    for (const genre in shelves) {
+      const bookIndex = shelves[genre].findIndex(book => book.title === title);
+      if (bookIndex !== -1) {
+        shelves[genre].splice(bookIndex, 1);
+        return `Yes, we have ${title}`;
+      }
+    }
+    return `Sorry, we do not have ${title}`;
+  }
+
+  calculateLateFee(daysLate) {
+    return Math.ceil(.25 * daysLate)
   }
 }
 
