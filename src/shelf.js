b01f61592b2eba36da719e73a7cd954f3393be88
Ryan Smith <106180616+RyanChrisSmith@users.noreply.github.com>
2023-02-26 12:08:59 -0700

    through iteration 2 of JS project for Mod1

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,36 @@
+function shelfBook(book, shelf) {
+  if (shelf.length < 3) {
+    shelf.unshift(book);
+  }
+    shelf.sort(function(a, b) {
+    a.title.localeCompare(b.title);
+  });
+}
+
+function unshelfBook(bookTitle, shelf) {
+  for (var i = 0; i < shelf.length; i++) {
+    if (shelf[i].title === bookTitle) {
+      shelf.splice(i, 1);
+      break;
+    }
+  }
+}
+
+function listTitles(shelf) {
+  return shelf.map(book => book.title).join(", ");
+}
+
+function searchShelf(shelf, title) {
+  return shelf.some(function(book) {
+    return book.title === title;
+  });
+}
+
+
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+  shelfBook,
+  unshelfBook,
+  listTitles,
+  searchShelf
 };
